aa556d74ae155641a5ef8717d0071a685ca863a6
Amir Rajan <ar@amirrajan.net>
2018-02-19 15:10:23 -0600

    make it so.

---
+++ vendor/CYAlert/CYAlert.m
@@ -0,0 +1,15 @@
+// CYAlert.m
+#import "CYAlert.h"
+
+@implementation CYAlert
+
++ (void)show {
+    UIAlertView *alert = [[UIAlertView alloc] init];
+    alert.title = @"This is Objective-C";
+    alert.message = @"Mixing and matching!";
+    [alert addButtonWithTitle:@"OK"];
+    [alert show];
+    [alert release];
+}
+
+@end
